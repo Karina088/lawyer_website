486f0f214227bb73124eb671880df17e12400343
kary_ser <morskya.life@gmail.com>
2023-07-14 18:43:44 +0300

    changed menu js

--- js/menuEl.js
+++ js/menuEl.js
@@ -1,13 +1,8 @@
 'use strict';
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () { getStickyMainMenu() }
 
 const menuEl = document.getElementById("menu")
-
-// Get the offset position of the navbar
 const stickyEl = menuEl.offsetTop
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function getStickyMainMenu() {
     if (window.pageYOffset >= stickyEl) {
         menuEl.classList.add('sticky')
@@ -15,3 +10,24 @@ function getStickyMainMenu() {
         menuEl.classList.remove('sticky')
     }
 }
+
+window.addEventListener('scroll',  () => { 
+    getStickyMainMenu() 
+})
+
+// When the user scrolls the page, execute myFunction
+// window.onscroll = function () { getStickyMainMenu() }
+
+// const menuEl = document.getElementById("menu")
+
+// Get the offset position of the navbar
+// const stickyEl = menuEl.offsetTop
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function getStickyMainMenu() {
+//     if (window.pageYOffset >= stickyEl) {
+//         menuEl.classList.add('sticky')
+//     } else {
+//         menuEl.classList.remove('sticky')
+//     }
+// }
